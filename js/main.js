fae26cb94cafca4bc451f8c50d45f0bf5321b544
marialapc <marialopez.apc@gmail.com>
2022-10-20 11:10:21 +0200

    variables

--- js/main.js
+++ js/main.js
@@ -7,17 +7,42 @@ const form = formElement.classList.remove("collapsed");
 
 const list = listElement
 
-/*Buscar solución problemas*/
-const kittenOne = `<img class="card_img"
-src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">British Shorthair</h4>
-<p class="card_description">
-Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-Es una maravilla acariciarle!
-</p>`;
+//variables de la información de cada gatito
+const kittenOneImage = '<img class="card_img"
+src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />';
+const kittenOneName = '<h3 class="card_title">Fiona</h3>';
+const kittenOneDesc = '<p class="card_description"> Risueño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p>';
+const kittenOneRace = '<h4 class="card_race">British Shorthair</h4>';
+
+
+const kittenOne = 
+
+
 
 const kittenli = `<li class ="kittenOne">`;
 const kitten1 = document.querySelector('.card.kittenOne');
 kitten1.innerHTML = kittenOne;
+
+//kitten2
+const kittenTwoImage = '<img class="card_img""src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />';
+const kittenTwoName = '<h3 class="card_title">Fiona</h3>';
+const kittenTwoDesc = '<p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p>';
+const kittenTwoRace = '<h4 class="card_race">British Shorthair</h4>';
+
+const kittenli2 = `<li class ="kittenTwo">`;
+const kitten2 = document.querySelector('.card.kittenTwo');
+kitten2.innerHTML = kittenTwoImage;
+
+//kitten3
+const kittenTwoImage = '<img class="card_img" src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg" alt="gatito" />';
+const kittenTwoName = '<h3 class="card_title">Cielo</h3>';
+const kittenTwoDesc = '<p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p>';
+const kittenTwoRace = '<h4 class="card_race">British Shorthair</h4>';
+
+const kittenThree = 
+
+const kittenli3 = `<li class ="kittenThree">`;
+const kitten3 = document.querySelector('.card.kittenThree');
+kitten3.innerHTML = kittenThree;
+
 
