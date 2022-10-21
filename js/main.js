425ec1470d1f1e5216e03399a3960294651cf77f
Aliripoll <aripollortega@gmail.com>
2022-10-21 11:16:17 +0200

    add kitten

--- js/main.js
+++ js/main.js
@@ -2,47 +2,63 @@
 
 const formElement = document.querySelector('.js-new-form');
 const listElement = document.querySelector('.js-list');
-
+//Mirar para crear un classList.add para añadir todos los gatos
 const form = formElement.classList.remove("collapsed");
 
-const list = listElement
 
 //variables de la información de cada gatito
-const kittenOneImage = '<img class="card_img"
-src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />';
-const kittenOneName = '<h3 class="card_title">Fiona</h3>';
-const kittenOneDesc = '<p class="card_description"> Risueño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p>';
-const kittenOneRace = '<h4 class="card_race">British Shorthair</h4>';
+//kitten1
+const kittenOneImage = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
+const kittenOneName = 'Anastasio';
+const kittenOneDesc = 'Risueño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!';
+const kittenOneRace = 'British Shorthair';
 
+const kittenOne =  `<li class="card">
+                   <article>
+                   <img class="card_img" src="${kittenOneImage}"; alt="gatito"/>
+                   <h3 class="card_title">${kittenOneName}</h3>
+                   <h4 class="card_race">${kittenOneRace}</h4>
+                   <p class="card_description">${kittenOneDesc}</p>
+                   </article>
+                   </li>`
 
-const kittenOne = 
+listElement.innerHTML = kittenOne;
 
-
-
-const kittenli = `<li class ="kittenOne">`;
-const kitten1 = document.querySelector('.card.kittenOne');
-kitten1.innerHTML = kittenOne;
 
 //kitten2
-const kittenTwoImage = '<img class="card_img""src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" />';
-const kittenTwoName = '<h3 class="card_title">Fiona</h3>';
-const kittenTwoDesc = '<p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p>';
-const kittenTwoRace = '<h4 class="card_race">British Shorthair</h4>';
+const kittenTwoImage = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+const kittenTwoName = 'Fiona';
+const kittenTwoDesc = 'Risueño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!';
+const kittenTwoRace = 'British Shorthair';
 
-const kittenli2 = `<li class ="kittenTwo">`;
-const kitten2 = document.querySelector('.card.kittenTwo');
-kitten2.innerHTML = kittenTwoImage;
+const kittenTwo = `  <li class="card">
+                      <article>
+                       <img class="card_img" src="${kittenTwoImage}"; alt="gatito"/>
+                       <h3 class="card_title">${kittenTwoName}</h3>
+                       <h4 class="card_race">${kittenTwoRace}</h4>
+                       <p class="card_description">${kittenTwoDesc}</p>
+                      </article>
+                     </li>`
+
+
+listElement.innerHTML = kittenTwo;
 
 //kitten3
-const kittenTwoImage = '<img class="card_img" src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg" alt="gatito" />';
-const kittenTwoName = '<h3 class="card_title">Cielo</h3>';
-const kittenTwoDesc = '<p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!</p>';
-const kittenTwoRace = '<h4 class="card_race">British Shorthair</h4>';
+const kittenThreeImage = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg" alt="gatito';
+const kittenThreeName = 'Cielo';
+const kittenThreeDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!';
+const kittenThreeRace = 'British Shorthair';
 
-const kittenThree = 
+const kittenThree = `  <li class="card">
+                        <article>
+                         <img class="card_img" src="${kittenThreeImage}"; alt="gatito"/>
+                         <h3 class="card_title">${kittenThreeName}</h3>
+                         <h4 class="card_race">${kittenThreeRace}</h4>
+                         <p class="card_description">${kittenThreeDesc}</p>
+                        </article>
+                       </li>`
 
-const kittenli3 = `<li class ="kittenThree">`;
-const kitten3 = document.querySelector('.card.kittenThree');
-kitten3.innerHTML = kittenThree;
+
+listElement.innerHTML = kittenThree;
 
 
